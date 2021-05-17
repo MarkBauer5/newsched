2b7071476bf11344b28cb9016b1f7ec85e44a9f8
Garrett Vanhoy <gmvanhoy@gmail.com>
2021-05-17 17:29:08 -0400

    Working on agc2 -- not working as I would imagine.

--- kernels/include/gnuradio/kernels/cpu/agc2_ff.hpp
+++ kernels/include/gnuradio/kernels/cpu/agc2_ff.hpp
@@ -9,35 +9,41 @@ namespace analog {
 namespace cpu {
 
 /**
- * @brief high performance Automatic Gain Control class with
- * attack and decay rates.
+ * @brief Automatic Gain Control class with attack and decay rates.
  *
  * @ingroup level_controllers_blk
  *
  * @details For Power the absolute value of the complex number is used.
  *
  */
-struct agc_cc : kernel_interface {
+struct agc2_ff : kernel_interface {
 public:
     /**
-     * @brief Build a complex value AGC loop block with attack and decay rates.
+     * @brief Construct a new agc2 ff object
      *
-     * @param rate the update rate of the loop.
-     * @param reference reference value to adjust signal power to.
-     * @param gain initial gain value.
-     * @param max_gain maximum gain.
+     * @param attack_rate
+     * @param decay_rate
+     * @param reference
+     * @param gain
+     * @param max_gain
      */
-    agc_cc(float rate, float reference, float gain, float max_gain)
-        : _rate(rate), _reference(reference), _gain(gain), _max_gain(max_gain){};
+    agc2_ff(
+        float attack_rate, float decay_rate, float reference, float gain, float max_gain)
+        : _attack_rate(attack_rate),
+          _decay_rate(decay_rate),
+          _reference(reference),
+          _gain(gain),
+          _max_gain(max_gain){};
 
     void operator()(void* in_buffer, void* out_buffer, size_t num_items);
     void operator()(void* buffer, size_t num_items)
     {
         operator()(buffer, buffer, num_items);
-    }
+    };
 
 protected:
-    float _rate;
+    float _attack_rate;
+    float _decay_rate;
     float _reference;
     float _gain;
     float _max_gain;
---
+++ kernels/lib/cpu/agc2_ff.cpp
@@ -0,0 +1,38 @@
+#include <gnuradio/kernels/cpu/agc2_ff.hpp>
+#include <cmath>
+#include <iostream>
+
+namespace gr {
+namespace kernels {
+namespace analog {
+namespace cpu {
+
+void agc2_ff::operator()(void* in_buffer, void* out_buffer, size_t num_items)
+{
+    float* in = reinterpret_cast<float*>(in_buffer);
+    float* out = reinterpret_cast<float*>(out_buffer);
+
+    for (unsigned int i = 0; i < num_items; i++) {
+        out[i] = in[i] * _gain;
+
+        float diff = fabsf(out[i]) - _reference;
+        float rate = fabsf(diff) > _gain ? _attack_rate : _decay_rate;
+
+        // Adjust gain according to difference between signal level and reference
+        // Gain depends on attack or decay mode
+        _gain -= diff * rate;
+
+        // Negative gain is prohibited
+        _gain = _gain < 0.0f ? 0.0f : _gain;
+
+        // Clip the gain at _max_gain
+        _gain = _gain > _max_gain ? _max_gain : _gain;
+
+        std::cout << (_gain > _max_gain ? "Hit max gain" : "Not saturated.") << std::endl;
+    }
+}
+
+} // namespace cpu
+} // namespace analog
+} // namespace kernels
+} // namespace gr
